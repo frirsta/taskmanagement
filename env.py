657b0a1320e3c761887e4ab4bc4174dbdfa9c2f5
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2023-08-18 17:27:08 +0200

    reset database

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://hpuuwgzz:DSG795hNEuubJqcZ3ahiDRnfZJzFt9fn@snuffleupagus.db.elephantsql.com/hpuuwgzz'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://886343547657747:o7Qqwgm_LPtYPMVX8f2MROlSZJU@ddms7cvqu'
+os.environ['SECRET_KEY'] = 'secret_key_frirsta'
